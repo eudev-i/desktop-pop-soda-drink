42663783d373e0121e8334d86cba09a8ee39d59d
randerson3301 <17259228@CFP127ALUNO.LOCAL>
2019-05-14 09:03:42 -0300

    First commit

---
+++ src/br/com/senaijandira/app/App.java
@@ -0,0 +1,12 @@
+package br.com.senaijandira.app;
+
+import br.com.senaijandira.dbutils.Conexao;
+
+public class App {
+
+	public static void main(String[] args) {
+		Conexao.getConnection();
+		
+	}
+
+}
---
+++ src/br/com/senaijandira/dbutils/Conexao.java
@@ -0,0 +1,32 @@
+package br.com.senaijandira.dbutils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+	private static Connection con;
+
+	public static Connection getConnection() {
+
+		try {
+			// Tentando chamar o Driver do mariaDB
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			String dbURL = "jdbc:mysql://localhost:3306/db_popsoda?useTimezone=true&serverTimezone=UTC";
+			
+
+			con = DriverManager.getConnection(dbURL, "root", "bcd127");
+			
+			
+			System.out.println("Deu certo");
+			
+			
+		} catch (SQLException err) {
+			err.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return con;
+	}
+}
