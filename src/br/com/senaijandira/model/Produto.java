6945c7ac67c3866ca1e0cb6d96f408062d4f44b2
randerson3301 <17259228@CFP127ALUNO.LOCAL>
2019-05-14 09:44:25 -0300

    model and project structure added

---
+++ src/br/com/senaijandira/model/Produto.java
@@ -0,0 +1,28 @@
+package br.com.senaijandira.model;
+
+public class Produto {
+	
+	private int idProduto;
+	private String nome;
+	private double unidadeMedida;
+	private String descricao;
+	private String imagem;
+	private double valorUnitario;
+	private int qtdeFardo;
+	private int qtdeEstoque;
+	private double peso;
+	private double volume;
+	private int status;
+	private String localizacao;
+	private double ipi;
+	private double demandaMensal;
+	private int tempoRessuprimento;
+	private int intervaloRessuprimento;
+	private int estoqueSeguranca;
+	private int pontoRessuprimento;
+	private int loteCompras;
+	private int estoqueMaximo;
+	private int tipoProduto;
+	private int statusHome;
+
+}
